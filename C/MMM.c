0718b805f6a98b2b0a851d3a6c8c401f7b449722
Chittu13 <shanmukha.s.royal@gmail.com>
2022-11-10 09:10:04 +0530

    Updated code:

---
+++ C/MMM.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main()
+{
+    int n,arr[50],total=0;
+    float mean=0,median=0;
+    printf("Enter the number of elements: \n");
+    scanf("%d",&n);
+    printf("\nEnter the %d numbers: \n",n);
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        total+=arr[i];
+    }
+    float sum=total;
+    mean=(sum)/n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    if(n%2==0)
+    {
+        median=(n/2+((n/2)+1))/2;
+    }
+    else
+    {
+      median=(n+1)/2;
+    }
+    float fmedian=0;
+    fmedian=(int)arr[(int)median];
+    int maxValue = 0, maxCount = 0;
+   for (int i = 0; i < n; ++i) {
+      int count = 0;
+      
+      for (int j = 0; j < n; ++j) {
+         if (arr[j] == arr[i])
+         ++count;
+      }
+      
+      if (count > maxCount) {
+         maxCount = count;
+         maxValue = arr[i];
+      }
+   }
+    printf("Mean: %f\n",mean);
+    printf("median: %f\n",fmedian);
+    printf("Mode: %d\n",maxValue);
+}
