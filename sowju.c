0b06932151045cb4ccedb358675141d1f20fe8d3
K. Shanmukha Srinivasulu Royal <114333116+Chittu13@users.noreply.github.com>
2023-03-11 15:50:05 +0530

    Update sowju.c

--- sowju.c
+++ sowju.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 int main()
 {
-    int a[2][2],b[2][2],sum[2][2];
+    int a[2][2],b[2][2],sum[2][2],i,j;
     
-    printf("\nEnter elements of 1st matri2:\n");
-    for (int i=0;i<2;i++)
+    printf("\nEnter elements of 1st matrix:\n");
+    for ( i=0;i<2;i++)
     {
         for (int j=0;j<2;j++)
-        {   printf("arra2[%d][%d]: ",i,j);
+        {   printf("arra1[%d][%d]: ",i,j);
             scanf("%d",&a[i][j]);
         }
     }
-    printf("\nEnter elements of 2st matri2:\n");
-    for (int i=0;i<2;i++)
+    printf("\nEnter elements of 2st matrix:\n");
+    for ( i=0;i<2;i++)
     {
-        for (int j=0;j<2;j++)
+        for (j=0;j<2;j++)
         {   printf("arra2[%d][%d]: ",i,j);
             scanf("%d",&b[i][j]);
         }
@@ -23,14 +23,15 @@ int main()
     
     // adding of two matri2 
 
-    for (int i=0;i<2;i++)
+    for (i=0;i<2;i++)
     {
-        for (int j=0;j<2;j++)
+        for (j=0;j<2;j++)
         {   
           sum[i][j]=a[i][j]+b[i][j]; 
         }
     }
     printf("\n");
+    printf("Addition of two matrix: \n");
     for (int i=0;i<2;i++)
     {
         for (int j=0;j<2;j++)
